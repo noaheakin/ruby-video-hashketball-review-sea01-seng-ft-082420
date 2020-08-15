9532c7bd38ad21179e531377d529b77e01aeb1f3
Noah Eakin <noaheakin@gmail.com>
2020-08-15 02:03:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,5 +134,15 @@ def num_points_scored (player_name)
         return stats[:points]
       end
     end
-  end 
+  end
+end
+
+def shoe_size (player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |stats|
+      if stats[:player_name] == player_name
+        return stats[:shoe]
+      end
+    end
+  end
 end
